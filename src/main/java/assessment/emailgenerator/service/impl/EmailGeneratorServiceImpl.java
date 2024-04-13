82773636339ce3464873dd84d70a4a52a9f2457a
Fatme98 <69259691+Fatme98@users.noreply.github.com>
2024-04-13 20:41:44 +0300

    Add files via upload

---
+++ src/main/java/assessment/emailgenerator/service/impl/EmailGeneratorServiceImpl.java
@@ -0,0 +1,27 @@
+package assessment.emailgenerator.service.impl;
+
+import assessment.emailgenerator.models.view.Emails;
+import assessment.emailgenerator.models.view.GeneratedEmail;
+import assessment.emailgenerator.service.EmailGeneratorService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmailGeneratorServiceImpl implements EmailGeneratorService {
+    @Override
+    public Emails generateEmails(String input1, String input2, String input3, String input4, String input5, String input6, String expression) {
+        Emails emails = new Emails();
+        List<GeneratedEmail> generatedEmails = new ArrayList<>();
+        generatedEmails.add(emailGenerator(input1, input2, input3, input4, input5, input6, expression));
+        emails.setData(generatedEmails);
+        return emails;
+    }
+
+    private GeneratedEmail emailGenerator(String input1, String input2, String input3, String input4, String input5, String input6, String expression) {
+        GeneratedEmail generatedEmail = new GeneratedEmail(input1, input2);
+
+        return generatedEmail;
+    }
+}
