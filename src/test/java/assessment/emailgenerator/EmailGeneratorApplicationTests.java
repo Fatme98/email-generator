7ce9b1d3990102e552a27263aa896b1869edd402
Fatme98 <69259691+Fatme98@users.noreply.github.com>
2024-04-13 20:46:42 +0300

    Add files via upload

---
+++ src/test/java/assessment/emailgenerator/EmailGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package assessment.emailgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = EmailGeneratorApplicationTests.class)
+class EmailGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
